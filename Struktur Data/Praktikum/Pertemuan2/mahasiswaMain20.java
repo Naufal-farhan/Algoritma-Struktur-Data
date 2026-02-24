28e092247c0cc2ec68ddaa1ce747d828f9d036a1
Naufal-farhan <nefener123@gmail.com>
2026-02-24 20:25:42 +0700

    Percobaan2

---
+++ Struktur Data/Praktikum/Pertemuan2/mahasiswaMain20.java
@@ -0,0 +1,17 @@
+package Praktikum.Pertemuan2;
+
+public class mahasiswaMain20 {
+    public static void main(String[] args) {
+        mahasiswa20 mhs1 = new mahasiswa20();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "1231412";
+        mhs1.kelas = "TI 1-H";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("TI 1-G");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+    
+}
