a4b4ea99701c7d66342b04309982ee1cc9fd760b
Naufal-farhan <nefener123@gmail.com>
2026-02-25 12:55:45 +0700

    Latihan Praktikum2

---
+++ Struktur Data/Praktikum/DosenMain20.java
@@ -0,0 +1,21 @@
+package Praktikum;
+
+public class DosenMain20 {
+    public static void main(String[] args) {
+        Dosen20 dosen1 = new Dosen20();
+        dosen1.idDosen = "1414124";
+        dosen1.nama = "JAMAL KALAM";
+        dosen1.status = true;
+        dosen1.tahunBergabung = 2015;
+        dosen1.bidangKeahlian = "AGAMA";
+
+        dosen1.tampilkanInformasi();
+        dosen1.setStatus(false);
+        dosen1.hitungMasaKerja(2020);
+        dosen1.ubahKeahlian("SEPAK BOLA");
+        dosen1.tampilkanInformasi();
+
+        Dosen20 dosen2 = new Dosen20("21412551","ASEP HARUZO",false,2013,"PROGRAMMING");
+        dosen2.tampilkanInformasi();
+    }
+}
