547eafd484c902b63158903369d221f8adc1bffd
Naufal-farhan <nefener123@gmail.com>
2026-03-03 21:04:53 +0700

    Modifikasi Percobaan 2

--- Struktur Data/Praktikum/Pertemuan3/Mahasiswa20.java
+++ Struktur Data/Praktikum/Pertemuan3/Mahasiswa20.java
@@ -5,4 +5,12 @@ public class Mahasiswa20 {
     public String kelas;
     public float ipk;
     
+    void cetakinfo(int i){
+         System.out.println("Data Mahasiswa ke-"+(i+1)+" : ");
+            System.out.println("NIM   : "+ nim);
+            System.out.println("NAMA  : "+nama);
+            System.out.println("KELAS : "+kelas);
+            System.out.println("IPK   : "+ipk);
+            System.out.println("--------------------------------------------");
+    }
 }
--- Struktur Data/Praktikum/Pertemuan3/MahasiswaDemo20.java
+++ Struktur Data/Praktikum/Pertemuan3/MahasiswaDemo20.java
@@ -25,12 +25,7 @@ public class MahasiswaDemo20 {
         }
 
         for(int i = 0;i<3;i++){
-            System.out.println("Data Mahasiswa ke-"+(i+1)+" : ");
-            System.out.println("NIM   : "+arrayOfMahasiswa20[i].nim);
-            System.out.println("NAMA  : "+arrayOfMahasiswa20[i].nama);
-            System.out.println("KELAS : "+arrayOfMahasiswa20[i].kelas);
-            System.out.println("IPK   : "+arrayOfMahasiswa20[i].ipk);
-            System.out.println("--------------------------------------------");
+            arrayOfMahasiswa20[i].cetakinfo(i);
         }
     }
 }
