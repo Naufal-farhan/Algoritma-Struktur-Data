0be4892209d1eac3ade537ba81475f932ef5e068
Naufal-farhan <nefener123@gmail.com>
2026-05-10 19:16:14 +0700

    Percobaan1

---
+++ Struktur Data/Praktikum/Pertemuan10/QueueMain.java
@@ -0,0 +1,51 @@
+package Praktikum.Pertemuan10;
+
+import java.util.*;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan kapasistas Queue : ");
+        int n = sc.nextInt();
+        int pilih;
+        Queue Q = new Queue(n);
+
+        do {
+            menu();
+            System.out.print("Pilih Menu : ");
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan Data Baru!!... : ");
+                    int dataMasuk = sc.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data Yang Dikeluarkan : " + dataKeluar);
+                        break;
+                    }
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+    }
+
+    public static void menu() {
+        System.out.println("================ MASUKKAN OPERASI YANG INGIN DILAKUKAN ================");
+        System.out.println("1. Enque");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("-----------------------------------------------------------------------");
+    }
+}
